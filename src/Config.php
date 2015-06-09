f86b38b9e49b1f165ac5f02963db38a7213688f3
pedzed <pedzed@outlook.com>
2015-06-09 20:03:42 +0200

    Added PHPUnit test.

---
+++ src/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tres\config {
+    
+    class Config {
+        
+        
+        
+    }
+    
+}
---
+++ tests/ConfigTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Tres\config\Config;
+
+class ConfigTest extends PHPUnit_Framework_TestCase {
+    
+    public function testInit() {
+        $config = new Config();
+    }
+    
+}
