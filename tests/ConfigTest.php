3b0813771688a8095ce43188595634a32130ec37
ped zed <pedzed@outlook.com>
2015-06-12 23:00:02 +0200

    Updated tests.
    Prefixes now required.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -40,42 +40,54 @@ class ConfigTest extends PHPUnit_Framework_TestCase {
                 'klm',
                 'nop' => 'qrst',
             ],
+            'hij.nop.deeply.nested.array.with.null' => null,
+            'hij.nop.deeply.nested.array.with.false' => false,
         ];
         $config->addFromArray($value);
         
         $expectedValue = $value['abc.def'];
         
+        $actualValue = $config->get('abc.def');
+        $this->assertSame($expectedValue, $actualValue);
+        
         $actualValue = $config->get()['abc.def'];
         $this->assertSame($expectedValue, $actualValue);
         
-        $actualValue = $config->get('abc.def');
+        $actualValue = $config->get('abc')['def'];
+        $this->assertSame($expectedValue, $actualValue);
+        
+        $actualValue = $config->get()['abc.def'];
+        $this->assertSame($expectedValue, $actualValue);
+        
+        $actualValue = $config->get()['hij.klm'];
+        $this->assertSame($expectedValue, $actualValue);
+        
+        $actualValue = $config->get()['hij.nop'];
+        $this->assertSame($expectedValue, $actualValue);
+        
+        $expectedValue = [
+            'array' => [
+                'with' => [
+                    'null' => null,
+                    'false' => false,
+                ],
+            ],
+        ];
+        $actualValue = $config->get('hij.nop.deeply.nested');
         $this->assertSame($expectedValue, $actualValue);
     }
     
     public function testAdd() {
         $config = new Config();
-        
         $config->add('key123', 'value123');
         
         $expectedValue = 'value123';
         $actualValue = $config->get('key123');
-        
-        $this->assertSame($expectedValue, $actualValue);
-    }
-    
-    public function testAddWithPrefix() {
-        $config = new Config();
-        
-        $config->add('key123', 'value123', 'prefix123');
-        
-        $expectedValue = 'value123';
-        $actualValue = $config->get('prefix123.key123');
         $this->assertSame($expectedValue, $actualValue);
     }
     
     public function testAddFromArray() {
         $config = new Config();
-        
         $value = [
             'abc.def' => 17,
             'efg',
@@ -84,54 +96,17 @@ class ConfigTest extends PHPUnit_Framework_TestCase {
                 'nop' => 'qrst',
             ],
         ];
-        
         $config->addFromArray($value);
         
-        $expectedValue = $value;
-        $actualValue = $config->get();
-        $this->assertSame($expectedValue, $actualValue);
-    }
-    
-    public function testAddFromArrayWithPrefix() {
-        $config = new Config();
-        $value = [
-            'abc.def' => 17,
-            'efg',
-            'hij' => [
-                'klm',
-                'nop' => 'qrst',
-            ],
-        ];
-        $config->addFromArray($value, 'prefix123');
-        
-        $expectedValue = $value;
-        
-        $actualValue = $config->get('prefix123');
+        $expectedValue = ['def' => 17];
+        $actualValue = $config->get('abc');
         $this->assertSame($expectedValue, $actualValue);
     }
     
     public function testAddFromFile() {
         $config = new Config();
         
-        $expectedValue = [
-            'abc' => 'def',
-            'ghi' => 123,
-            'jk' => [
-                'lmnop',
-                14 => 'qrst',
-            ],
-        ];
-        
-        $config->addFromFile(self::CONFIG_DIR.'/config1.php');
-        
-        $actualValue = $config->get();
-        $this->assertSame($expectedValue, $actualValue);
-    }
-    
-    public function testAddFromFileWithPrefix() {
-        $config = new Config();
-        
-        $expectedValue = $this->_config1ValueFromFile;
+        $expectedValue = ['prefix123' => $this->_config1ValueFromFile];
         
         $config->addFromFile(self::CONFIG_DIR.'/config1.php', 'prefix123');
         
@@ -166,36 +141,6 @@ class ConfigTest extends PHPUnit_Framework_TestCase {
         $expectedValue = array_merge($this->_config1ValueFromFile, $this->_config2ValueFromFile);
         $actualValue = $config->get();
         
-        $this->assertSame($expectedValue, $actualValue);
-    }
-    
-    public function testAddFromDirectoryWithPrefix() {
-        $config = new Config();
-        
-        $expectedValue = [
-            'abc' => 'def',
-            'ghi' => 123,
-            'jk' => [
-                'lmnop',
-                14 => 'qrst',
-            ],
-            'uvw' => 'xyz',
-            0 => null,
-            null,
-            10,
-            'abc.true' => true,
-            'abc.false' => false,
-            'abc.string.true' => true,
-            'abc.string' => [
-                'def',
-                'ghi',
-                'klmnop' => 'q',
-            ],
-        ];
-        
-        $config->addFromDirectory(self::CONFIG_DIR, true);
-        
-        $actualValue = $config->get();
         $this->assertSame($expectedValue, $actualValue);
     }
     
